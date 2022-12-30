8193aa8614e7d75a60a0f035535ddb2badf5f823
yassine20011 <medalgamer47@gmail.com>
2022-12-31 00:36:07 +0100

    update

--- setup.py
+++ setup.py
@@ -19,9 +19,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        'topic :: Internet :: Proxy Servers',
-        
+        "Operating System :: OS Independent",        
     ],
     python_requires='>=3.6',
     license='MIT',
