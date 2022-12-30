fe9dec733034c0d2b6bca2f0bd8ff6ea7c6f6f5b
yassine20011 <medalgamer47@gmail.com>
2022-12-31 00:33:01 +0100

    v0.1.7

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="py-proxy-checker",
-    version="0.1.6",
+    version="0.1.7",
     author="Yassine Amjad",
     author_email="Yassine.amjad001@gmail.com",
+    url="https://github.com/yassine20011/py-proxy-checker",
     description="A simple checkerproxy library",
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -19,6 +20,9 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        'topic :: Internet :: Proxy Servers',
+        
     ],
     python_requires='>=3.6',
+    license='MIT',
     )
